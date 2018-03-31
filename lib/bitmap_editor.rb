5f9509f7439589b80bf8c33e237b9bc9e1adbe3a
Tom Dracz <tjdracz@gmail.com>
2018-03-31 17:03:04 +0100

    Add initial spec conditions for BitmapEditor class

--- lib/bitmap_editor.rb
+++ lib/bitmap_editor.rb
@@ -7,13 +7,16 @@ class BitmapEditor
     return puts "please provide correct file" if file.nil? || !File.exists?(file)
 
     File.open(file).each do |line|
-      line = line.chomp
-      case line
-      when 'S'
-          puts "There is no image"
-      else
-          puts 'unrecognised command :('
-      end
+      parse_input(line.chomp)
+    end
+  end
+
+  def parse_input(input_line)
+    case line
+    when 'S'
+      puts "There is no image"
+    else
+      puts 'unrecognised command :('
     end
   end
 end
--- spec/bitmap_editor_spec.rb
+++ spec/bitmap_editor_spec.rb
@@ -1,4 +1,38 @@
 require_relative "../lib/bitmap_editor"
 
 describe BitmapEditor do
+  describe "#parse_input" do
+    context "with I command" do
+      it "creates bitmap of specified width and height"
+    end
+
+    context "with C command" do
+      it "clears bitmap to its initial state"
+      it "returns an error if there is not bitmap"
+    end
+
+    context "with L command" do
+      it "colours the pixel at specified coordinates"
+      it "returns an error if there is no bitmap"
+    end
+
+    context "with V command" do
+      it "draws a vertical line of specified colour between given coordinates"
+      it "returns an error if there is no bitmap"
+    end
+
+    context "with H command" do
+      it "draws a vertical line of specified colour between given coordinates"
+      it "returns an error if there is no bitmap"
+    end
+
+    context "with S command" do
+      it "outputs the contents of the current image"
+      it "returns an error if there is no bitmap"
+    end
+
+    context "with unrecognised command" do
+      it "returns an error message about unrecognised command"
+    end
+  end
 end
