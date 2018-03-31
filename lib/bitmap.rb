7510b92d78ce4c353bc56590b8fdba8443d07902
Tom Dracz <tjdracz@gmail.com>
2018-03-31 15:41:31 +0100

    Add pixel matrix creation and error checks for invalid arguments

--- lib/bitmap.rb
+++ lib/bitmap.rb
@@ -1,15 +1,26 @@
+class InvalidWidthError < StandardError
+end
+
+class InvalidHeightError < StandardError
+end
+
+
 class Bitmap
   attr_reader :width, :height, :pixels
 
   def initialize(width, height)
     @width = width
     @height = height
+    raise InvalidWidthError, "width must be an integer" unless @width.is_a?(Integer)
+    raise InvalidHeightError, "height must be an integer" unless @height.is_a?(Integer)
+    raise InvalidWidthError, "invalid width specified" unless @width.between?(1,250)
+    raise InvalidHeightError, "invalid height specified" unless @height.between?(1,250)
     @pixels = build_pixels
   end
 
   private
 
   def build_pixels
-    []
+    Array.new(@width) { Array.new(@height) { "O" } }
   end
 end
--- spec/bitmap_spec.rb
+++ spec/bitmap_spec.rb
@@ -2,16 +2,44 @@ require_relative '../lib/bitmap'
 
 describe Bitmap do
   describe "#initialize" do
-    it "sets width and height of bitmap" do
-      bitmap = described_class.new(2,2)
-      expect(bitmap.width).to eq(2)
-      expect(bitmap.height).to eq(2)
+    context "with valid arguments" do
+      it "sets width and height of bitmap" do
+        bitmap = described_class.new(2,2)
+        expect(bitmap.width).to eq(2)
+        expect(bitmap.height).to eq(2)
+      end
+
+      it "sets the pixels matrix" do
+        bitmap = described_class.new(3,5)
+        pixel_matrix = Array.new(3) { Array.new(5) { "O" } }
+        expect(bitmap.pixels).to eq(pixel_matrix)
+      end
     end
 
-    it "sets the pixels matrix" do
-      bitmap = described_class.new(2,2)
-      pixel_matrix = Array.new(2) { Array.new(2) { "O" } }
-      expect(bitmap.pixels).to eq(pixel_matrix)
+    context "with invalid arguments" do
+      it "raises an error if width is out of min and max range" do
+        expect { described_class.new(0,134) }.to raise_error(InvalidWidthError)
+        expect { described_class.new(251,134) }.to raise_error(InvalidWidthError)
+        expect { described_class.new(-100,134) }.to raise_error(InvalidWidthError)
+      end
+
+      it "raises an error if height is out of min and max range" do
+        expect { described_class.new(134,0) }.to raise_error(InvalidHeightError)
+        expect { described_class.new(120,278) }.to raise_error(InvalidHeightError)
+        expect { described_class.new(120,-5) }.to raise_error(InvalidHeightError)
+      end
+
+      it "raises an error if width is not an integer" do
+        expect { described_class.new("abc",0) }.to raise_error(InvalidWidthError)
+        expect { described_class.new(nil,278) }.to raise_error(InvalidWidthError)
+        expect { described_class.new([],-5) }.to raise_error(InvalidWidthError)
+      end
+
+      it "raises an error if height is not an integer" do
+        expect { described_class.new(134,"foo") }.to raise_error(InvalidHeightError)
+        expect { described_class.new(120,[]) }.to raise_error(InvalidHeightError)
+        expect { described_class.new(120,nil) }.to raise_error(InvalidHeightError)
+      end
     end
   end
 end
